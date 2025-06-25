eeb9534e8d185edcf98be6f683c7f8c64f9ecc86
XeonMations <62395746+XeonMations@users.noreply.github.com>
2025-06-25 17:52:53 +0300

    seeing if this works (#3)
    * [NO GBP] Fixes narcolepsy's sleep_chance var not resetting (#91801)
    
    Co-authored-by: MrMelbert <51863163+MrMelbert@users.noreply.github.com>
    
    * Automatic changelog for PR #91801 [ci skip]
    
    * Update install_byond.sh
    
    * aaa
    
    ---------
    
    Co-authored-by: theOOZ <sheepwiththemask@gmail.com>
    Co-authored-by: MrMelbert <51863163+MrMelbert@users.noreply.github.com>
    Co-authored-by: tgstation-ci[bot] <179393467+tgstation-ci[bot]@users.noreply.github.com>

--- tools/ci/download_byond.sh
+++ tools/ci/download_byond.sh
@@ -2,4 +2,4 @@
 set -e
 source dependencies.sh
 echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR"
-curl -H "User-Agent: tgstation/1.0 CI Script" "http://spacestation13.github.io/byond-builds/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
+curl -H "User-Agent: The-Final-Nights/3.0 CI Script" "https://spacestation13.github.io/byond-builds/$BYOND_MAJOR/$BYOND_MAJOR.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
